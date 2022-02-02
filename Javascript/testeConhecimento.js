a715116d85ee6db1c8d4bc932c33cbf8bb0b3149
Allyson-Santana <santana.allyson459@gmail.com>
2022-02-02 08:01:46 -0300

    Revisão de javascript

--- Javascript/testeConhecimento.js
+++ Javascript/testeConhecimento.js
@@ -1,1 +1,10 @@
+const quotos = 'aAc';
+console.log(quotos.indexOf('b' && 'c'));
 
+const hasUpper = (str) => (/[A-Z]/).test(str);
+const hasLower = (str) => (/[a-z]/).test(str);
+const hasNumber = (str) =>(/[0-9]/).test(str);
+
+console.log(hasUpper('vc'));
+console.log(hasLower('VC'));
+console.log(hasNumber('abjnb'));
