f57239ca2592cdbda3f3a2d7ab0e2af6768d84a5
Allyson-Santana <santana.allyson459@gmail.com>
2022-07-30 20:35:16 -0300

    corrige conexão com o pg

--- typescript/simple-postgreSQL/src/app.ts
+++ typescript/simple-postgreSQL/src/app.ts
@@ -1,12 +1,8 @@
 import express from 'express'
 import cors from 'cors'
-import { DataSource } from "typeorm"
-import { join } from 'path';
 
 import routes from './routes'
-import * as dotenv from 'dotenv'
-
-dotenv.config();
+import { PostgresDataSource } from './database/connection'
 
 class App {
   public express: express.Application
@@ -25,24 +21,11 @@ class App {
   }
 
   private database (): void {
-    const PostgresDataSource = new DataSource({
-      type: 'postgres',
-      host: process.env.TYPEORM_DB_HOST,
-      port: Number(process.env.TYPEORM_DB_PORT),
-      username: process.env.TYPEORM_DB_USERNAME,
-      password: process.env.TYPEORM_DB_PASSWORD,
-      database: process.env.TYPEORM_DB_DATABASE,
-      logging: ["error", "log"],
-      entities: [join(__dirname, './entities/*{.ts,.js}')],
-      migrations: [join(__dirname, './database/migrations/*{.ts,.js}')]
+    PostgresDataSource.initialize().then(() => {
+      console.log('Data Source has been initialized!')
+    }).catch((err) => {
+      console.error('Error during Data Source initialization', err)
     })
-    PostgresDataSource.initialize()
-      .then(() => {
-          console.log("Data Source has been initialized!")
-        })
-      .catch((err) => {
-          console.error("Error during Data Source initialization", err)
-      })
   }
 
   private routes (): void {
