428740a13b00a816f4de037f1b4a136801b66122
Allyson-Santana <santana.allyson459@gmail.com>
2022-12-10 20:03:59 -0300

    correção de banco de dados

--- php/php_B7/_projects/registro/mysql.sql
+++
@@ -1,6 +0,0 @@
-create table tb_user (
-	cd_user int not null auto_increment,
-    nm_email varchar(40) not null,
-    nm_password varchar(255) not null,
-		constraint pk_user primary key(cd_user)
-);
